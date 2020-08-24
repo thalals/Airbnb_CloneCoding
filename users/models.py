bcca87054732b8c58bccf2a8400b68c85a789e00
thalals <thalsal@naver.com>
2020-08-24 15:00:43 +0900

    [add] users 앱 models.py (booleanField, dateField, charFiled-choices)

--- users/models.py
+++ users/models.py
@@ -9,12 +9,20 @@ class User(AbstractUser):
     GENDER_MALE = "male"
     GENDER_FEMALE = "female"
     GENDER_OTHER = "other"
+
+    Language_korean = "kr"
+    Language_english = "en"
     # tuple
     GENDER_CHOICES = (
         (GENDER_MALE, "Male"),
         (GENDER_FEMALE, "Female"),
         (GENDER_OTHER, "Other"),
     )
+    LANGUAGE_CHOICES = (
+        (Language_korean, "korean"),
+        (Language_english, "English"),
+    )
+
     avatar = models.ImageField(
         null=True, blank=True
     )  # black 는 db상이 아니라 form에서 사용 값이 없어도 괜찮게
@@ -26,3 +34,8 @@ class User(AbstractUser):
         default=GENDER_MALE,  # choices 의 default 값
     )
     bio = models.TextField(default="", blank=True)  # 값을 비워두지 않고 defalut 값생성(defalut="")
+    birthdate = models.DateField(null=True)
+    Language = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=10, null=True, blank=True
+    )
+    superhost = models.BooleanField(default=False)
