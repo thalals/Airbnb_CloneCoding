a489629dbb1ca3354a6325be0e4a0a06ddf410ed
thalals <thalsal@naver.com>
2020-08-24 15:45:39 +0900

    [add] UserAdmin.Filedsets 사용

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from . import models
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 
+# decorator model 바로 위에 써줘야함
+@admin.register(models.User)  # admin.ModelAdmin
+class CustomUserAdmin(UserAdmin):
 
-@admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
-    pass
+    """ Custom User Admin """
+
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {"fields": ("avatar", "gender", "birthdate", "bio", "superhost")},),
+    )
