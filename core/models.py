a7e85458f9bf4f41039b5936555df1d681420461
thalals <thalsal@naver.com>
2020-08-26 02:34:44 +0900

    [add] coreapp models DateTimeFiled auto_

--- core/models.py
+++ core/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class TimeStampedModel(models.Model):
-    created = models.DateTimeField()
-    updated = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)   #auto_now(자동 디비 업데이트)
+    updated = models.DateTimeField(auto_now=True)
     # 이 모델을 db에 추가하지 않기위해 추상모델로 만듬
     class Meta:
         abstract = True
