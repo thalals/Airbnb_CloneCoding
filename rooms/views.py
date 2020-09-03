2ae188ee0d937459be0b50e74c9bcfc31b5fb2d2
thalals <thalsal@naver.com>
2020-09-03 21:36:50 +0900

    #10.4 base.html(block), partials folder(header, footer.html)

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse  # httpresponse 이용
-
+from . import models
 
 def all_rooms(request):
-
-    return HttpResponse(content="hello")
+    all_rooms = models.Room.objects.all()
+    return render(request,"home.html", {"rooms" : all_rooms})
