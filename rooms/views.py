063cb1415a969029c1e28c659edd3edc7f9911f4
thalals <thalsal@naver.com>
2020-09-04 01:16:43 +0900

    #11.2 home.html (page_number)

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,17 @@
+from math import ceil   #올림
 from django.shortcuts import render
 from django.http import HttpResponse  # httpresponse 이용
 from . import models
 
+
 def all_rooms(request):
-    all_rooms = models.Room.objects.all()
-    return render(request,"home.html", {"rooms" : all_rooms})
+    page = int(request.GET.get("page",1))
+    page = int(page or 1)   #기본값 주기
+    page_size = 10
+    limit = page_size*page
+    offset = limit-page_size
+    all_rooms = models.Room.objects.all()[offset:limit]  # [start:finish] 제한하여 필터링
+    
+    page_count = ceil(models.Room.objects.count()/page_count)   #올림
+    
+    return render(request, "rooms/home.html", {"rooms": all_rooms, "page" : page, "page_count" : (page_count), "page_range" : range(1,page_count)})
