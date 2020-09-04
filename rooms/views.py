239429dd06eb987c694713a961cfda4110514cc1
thalals <thalsal@naver.com>
2020-09-04 15:33:57 +0900

    #11.6 paginaion 사용

--- rooms/views.py
+++ rooms/views.py
@@ -1,17 +1,18 @@
-from math import ceil   #올림
-from django.shortcuts import render
+from math import ceil  # 올림
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
 from django.http import HttpResponse  # httpresponse 이용
 from . import models
 
 
 def all_rooms(request):
-    page = int(request.GET.get("page",1))
-    page = int(page or 1)   #기본값 주기
-    page_size = 10
-    limit = page_size*page
-    offset = limit-page_size
-    all_rooms = models.Room.objects.all()[offset:limit]  # [start:finish] 제한하여 필터링
-    
-    page_count = ceil(models.Room.objects.count()/page_count)   #올림
-    
-    return render(request, "rooms/home.html", {"rooms": all_rooms, "page" : page, "page_count" : (page_count), "page_range" : range(1,page_count)})
+    page = request.GET.get("page")
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10)  # 리스트와, 페이지수
+    # 오류처리
+    try:
+        rooms = paginator.get_page(page)
+        return render(request, "rooms/home.html", {"rooms": rooms})
+
+    except EmptyPage:
+        return redirect("/")
