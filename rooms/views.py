a0aacea873ff9fa9ef532bfc25715e8c2e7bc82b
thalals <thalsal@naver.com>
2020-09-05 00:50:03 +0900

    #11.8 CBV

--- rooms/views.py
+++ rooms/views.py
@@ -1,18 +1,38 @@
 from math import ceil  # 올림
 from django.shortcuts import render, redirect
+from django.utils import timezone
 from django.core.paginator import Paginator, EmptyPage
 from django.http import HttpResponse  # httpresponse 이용
+from django.views.generic import ListView  # CBV
 from . import models
 
 
-def all_rooms(request):
-    page = request.GET.get("page")
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)  # 리스트와, 페이지수
-    # 오류처리
-    try:
-        rooms = paginator.get_page(page)
-        return render(request, "rooms/home.html", {"rooms": rooms})
+# def all_rooms(request):
+#     page = request.GET.get("page")
+#     room_list = models.Room.objects.all()
+#     paginator = Paginator(room_list, 10)  # 리스트와, 페이지수
+#     # 오류처리
+#     try:
+#         rooms = paginator.get_page(page)
+#         return render(request, "rooms/home.html", {"rooms": rooms})
 
-    except EmptyPage:
-        return redirect("/")
+#     except EmptyPage:
+#         return redirect("/")
+
+
+class HomeView(ListView):
+
+    """ Homeview Definition """
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"  # ordering은 필드를 반환해야함
+    context_object_name = "rooms"  # cbv가 제공하는 object 이름을 바꿈
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
+
